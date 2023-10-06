33b3f4fe893430f1539896e9fa695d2a73f642f6
Kotik-PC <freeman.maybe@gmail.com>
2023-10-06 17:01:39 +0700

    [Add] Load from appsettings.json

--- Program.cs
+++ Program.cs
@@ -1,45 +1,41 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Linq;
+using Microsoft.Extensions.Configuration;
+using SQL_Test;
 
-namespace SQL_Test
+IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+var options = new DbContextOptionsBuilder<AEXSoftDbContext>()
+    .UseSqlServer(connectionString)
+    .Options;
+
+using (var context = new AEXSoftDbContext(options))
 {
-    class Program
+    List<Customer> customers = context.Customer.ToList();
+
+    foreach (var customer in customers)
     {
-        static void Main(string[] args)
-        {
-            string connectionString = "Server=DESKTOP-HI96GF8;Database=AEXSoft;Trusted_Connection=True;Encrypt=False;";
-            
-            var options = new DbContextOptionsBuilder<AEXSoftDbContext>()
-                .UseSqlServer(connectionString)
-                .Options;
-
-            using (var context = new AEXSoftDbContext(options))
-            {
-                List<Customer> customers = context.Customer.ToList();
-
-                foreach (var customer in customers)
-                {
-                    Console.WriteLine($"ID: {customer.ID}, Name: {customer.Name}");
-                }
-            }
-
-            Console.WriteLine();
-
-            using (var context = new AEXSoftDbContext(options))
-            {
-                DateTime beginDate = new DateTime(2023, 01, 01);
-                decimal sumAmount = 1000;
-
-                var customers = context.GetCustomers(beginDate, sumAmount);
-
-                foreach (var customer in customers)
-                {
-                    Console.WriteLine($"Customer: {customer.CustomerName}, Manager: {customer.ManagerName}, Amount: {customer.Amount}");
-                }
-            }
-
-            Console.WriteLine();
-        }
+        Console.WriteLine($"ID: {customer.ID}, Name: {customer.Name}");
     }
 }
+
+Console.WriteLine();
+
+using (var context = new AEXSoftDbContext(options))
+{
+    DateTime beginDate = new DateTime(2023, 01, 01);
+    decimal sumAmount = 1000;
+
+    var customers = context.GetCustomers(beginDate, sumAmount);
+
+    foreach (var customer in customers)
+    {
+        Console.WriteLine($"Customer: {customer.CustomerName}, Manager: {customer.ManagerName}, Amount: {customer.Amount}");
+    }
+}
+
+Console.WriteLine();
